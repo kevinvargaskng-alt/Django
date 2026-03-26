781c5853a27e4da8d16ddf7281f47e0e6c7577f8
harvetpoveda29-hue <harvetpoveda29@gmail.com>
2026-03-26 14:50:04 -0500

    feat: agregar clase disciplinas

--- interfichas/models.py
+++ interfichas/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+
+class Disciplina(models.Model):
+    nombre_disciplina = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.nombre_disciplina
+    
 class TorneoInterfichas(models.Model):
     codigo_torneo_fichas = models.AutoField(primary_key=True)
     nombre_torneo = models.CharField(max_length=100)
